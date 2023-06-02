742a009d8432d935ed66f393db0ae9516b309e45
Lorenzo Busato <lorenzobusato07@gmail.com>
2023-06-02 15:50:00 -0300

    Modify: realizo cambios en la estetica de updateitem

--- sat-tdi/src/routes/stock/UpdateItem.js
+++ sat-tdi/src/routes/stock/UpdateItem.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useLocation, useNavigate } from "react-router-dom";
-import NavBar from './NavBar';
+import MainNavBar from '../orders/MainNavBar';
 
 function UpdateItem() {
 
@@ -63,50 +63,54 @@ function UpdateItem() {
     }
 
   return (
-    <div className=' min-h-screen'>
-        <NavBar />
-        <h1 className="flex justify-center text-5xl">Editar producto</h1>
-        <div className='max-w-7xl mx-auto p-4 text-center'>
-            <p className='font-bold'>NOMENCLATURA PARA EL INGRESO DE PRODUCTOS:</p>
-            <p>
-                Para el ingreso de un nuevo producto es de suma importancia 
-                que se respeten las normas de nomenclatura para poder replicarse
-                en todo el sistema.
-            </p>
-            <p>
-                En nuestro caso el orden es el siguiente:
-            </p>
-            <p className='font-bold'>
-                (Nombre del repuesto) (Calidad) (Modelo) (Color)
-            </p>
-            <p>
-                Teniendo la primer letra del nombre, de la calidad y del color en 
-                MAYUSCULA y respetando el nombre del modelo
-            </p>
-        </div>
-        <div className="p-4 max-w-md mx-auto">
-            <form onSubmit={handleSubmit} className="mb-4">
-                <div className="mb-2">
-                    <label className="block text-gray-700 font-bold mb-2" htmlFor="repuesto">Nombre del repuesto:</label>
-                    <input 
-                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
-                        type="text" 
-                        id="repuesto" 
-                        value={repuesto} 
-                        onChange={handleRepuestosChange} 
-                    />
+    <div className='bg-gray-300 min-h-screen pb-2'>
+        <MainNavBar />
+        <div className='bg-white m-2 py-8 px-2'>
+            <h1 className="text-2xl font-bold text-center">Editar producto</h1>
+            <div>
+                <div className='max-w-7xl mx-auto p-4 text-center'>
+                    <p className='font-bold'>NOMENCLATURA PARA EL INGRESO DE PRODUCTOS:</p>
+                    <p>
+                        Para el ingreso de un nuevo producto es de suma importancia 
+                        que se respeten las normas de nomenclatura para poder replicarse
+                        en todo el sistema.
+                    </p>
+                    <p>
+                        En nuestro caso el orden es el siguiente:
+                    </p>
+                    <p className='font-bold'>
+                        (Nombre del repuesto) (Calidad) (Modelo) (Color)
+                    </p>
+                    <p>
+                        Teniendo la primer letra del nombre, de la calidad y del color en 
+                        MAYUSCULA y respetando el nombre del modelo
+                    </p>
                 </div>
-                <div className='flex justify-between px-10'>
-                    <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-                        Guardar
-                    </button>
-                    <button 
-                    className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                    onClick={() => { navigate(`/items`) }} >
-                        Volver
-                    </button>
+                <div className="p-4 max-w-md mx-auto">
+                    <form onSubmit={handleSubmit} className="mb-4">
+                        <div className="mb-2">
+                            <label className="block text-gray-700 font-bold mb-2" htmlFor="repuesto">Nombre del repuesto:</label>
+                            <input 
+                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+                                type="text" 
+                                id="repuesto" 
+                                value={repuesto} 
+                                onChange={handleRepuestosChange} 
+                            />
+                        </div>
+                        <div className='flex justify-between px-10'>
+                            <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                                Guardar
+                            </button>
+                            <button 
+                            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                            onClick={() => { navigate(`/items`) }} >
+                                Volver
+                            </button>
+                        </div>
+                    </form>
                 </div>
-            </form>
+            </div>
         </div>
     </div>
   );
